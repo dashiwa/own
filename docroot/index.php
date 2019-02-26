3d5f6b2bcf4ba8fb6210a840d1e904da18e6ef50
Raman_Starshykh <Raman_Starshykh@epam.com>
2019-02-26 11:48:10 +0300

    S-07 Creating the Front Controller

--- docroot/index.php
+++ docroot/index.php
@@ -1,8 +1,17 @@
 <?php
-
 // index.php
+
+// load and initialize any global libraries
 require_once 'model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
-
-require 'templates/list.php';
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ('/index.php' === $uri) {
+  list_action();
+} elseif ('/index.php/show' === $uri && isset($_GET['id'])) {
+  show_action($_GET['id']);
+} else {
+  header('HTTP/1.1 404 Not Found');
+  echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
