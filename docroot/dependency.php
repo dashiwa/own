c7cfb108cb11a3c95e079a0e2d594001f9afcdb6
Raman_Starshykh <Raman_Starshykh@epam.com>
2019-02-27 11:27:10 +0300

    S-09 Dependency injection - First step

---
+++ docroot/dependency.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class SessionStorage
+{
+  function __construct($cookieName = 'PHP_SESS_ID')
+  {
+    session_name($cookieName);
+    session_start();
+  }
+
+  function set($key, $value)
+  {
+    $_SESSION[$key] = $value;
+  }
+
+  function get($key)
+  {
+    return $_SESSION[$key];
+  }
+
+  // ...
+}
+
+
+class User
+{
+  protected $storage;
+
+  function __construct()
+  {
+    $this->storage = new SessionStorage();
+  }
+
+  function setLanguage($language)
+  {
+    $this->storage->set('language', $language);
+  }
+
+  function getLanguage()
+  {
+    return $this->storage->get('language');
+  }
+
+  // ...
+}
+
+$user = new User();
+$user->setLanguage('fr');
+$user_language = $user->getLanguage();
