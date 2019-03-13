b7351d3a14aa00e8794a210b26d1b3a30a1ddfa5
Raman_Starshykh <Raman_Starshykh@epam.com>
2019-03-13 13:45:03 +0300

    S-10 Own PHP - Front controller

---
+++ web/bye.php
@@ -0,0 +1,12 @@
+<?php
+
+// framework/bye.php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$response = new Response('Goodbye!');
+$response->send();
