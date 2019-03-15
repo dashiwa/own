cf2f88fbeed7fc988a8a87a3206e18f88c1a455b
Raman_Starshykh <Raman_Starshykh@epam.com>
2019-03-15 11:33:28 +0300

    SR-15 Add leap year function

--- src/app.php
+++ src/app.php
@@ -1,22 +1,26 @@
 <?php
 
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
+// example.com/src/app.php
+use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
 
-$routes = new RouteCollection();
+function is_leap_year($year = null) {
+  if (null === $year) {
+    $year = date('Y');
+  }
 
-$routes->add('hello', new Route('/hello/{name}', [
-  'name' => 'World',
+  return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+  'year' => null,
   '_controller' => function ($request) {
-    // $foo will be available in the template
-    $request->attributes->set('foo', 'bar');
+    if (is_leap_year($request->attributes->get('year'))) {
+      return new Response('Yep, this is a leap year!');
+    }
 
-    $response = render_template($request);
-
-    // change some header
-    $response->headers->set('Content-Type', 'text/plain');
-
-    return $response;
+    return new Response('Nope, this is not a leap year.');
   }
 ]));
 
